c697106420699be7c37333f2bb846e078ff2c283
Gamal Abdul Aziz <gamal.aziz1000@gmail.com>
2026-09-19 12:10:00 +0700

    Revert "feat: add CORS headers to server configuration for improved API access"
    This reverts commit f92c42c6f2643da8bf5bd3594c869e10afec74ab.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,18 +5,9 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  },
   integrations: [react()],
   vite: {
     server: {
-      cors: true,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
       proxy: {
         '/eve': {
           target: 'http://127.0.0.1:2000',
